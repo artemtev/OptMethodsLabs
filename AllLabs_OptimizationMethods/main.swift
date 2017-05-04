63fbd6c9620bd3e05d9b7b1260dfc11ee68afe85
lanzeron <artemtevosyan1@gmail.com>
2017-05-04 23:10:06 +0300

    Implemented 10 labs.

--- AllLabs_OptimizationMethods/main.swift
+++ AllLabs_OptimizationMethods/main.swift
@@ -21,6 +21,7 @@ func secondDerivative(_ x: Double) -> Double {
     return (6.0*pow((3.0 - x),4.0))/pow((x + 5.0), 4.0) + (16.0*pow((3.0 - x), 3.0))/pow((x + 5.0), 3.0) + (12.0*pow((3.0 - x), 2.0))/pow((x + 5.0), 2.0) - 1.0/(4.0*pow((x + 2.0),(3.0/2.0)))
 }*/
 
+
 func function(_ x: Double) -> Double {
     return (2-pow(x, 2.0))*sin(x+0.5)-(2*cos(x+0.5)+1)*x
 }
